bdf8c0c1905df750bdcddc4faa8f9c6654c666fc
marcassim <marcelo.assim@gmail.com>
2022-07-15 12:04:25 -0300

    Criação da base de dados; Preparação do ambiente admin;

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,26 @@
+from django import forms
 from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList
+from django.db.models import Avg, Sum
+from django.utils.html import format_html
 
-# Register your models here.
+from .models import Cliente, Proposta
+
+
+class PropostaAdmin(admin.ModelAdmin):
+    
+    # def get_changelist(self, request, **kwargs):
+    #     return TotalChangeList
+
+    list_display = ['cliente','data','pago','valor']
+    search_field=['cliente']
+    list_filter=['cliente','data','situacao']
+    # lista_editable=['valor']
+    lista_per_page=30
+    save_on_top=True
+
+class ClienteAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Proposta, PropostaAdmin)
+admin.site.register(Cliente, ClienteAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,80 @@
-from django.db import models
+# -*- Mode: Python; coding: utf-8 -*-
+from __future__ import unicode_literals
 
-# Create your models here.
+from django.contrib import admin
+from django.contrib.admin.views.main import ChangeList
+from django.db import models
+from django.db.models import Avg, Sum
+from django.utils.html import format_html
+from django.utils.safestring import mark_safe
+from localflavor.br.br_states import STATE_CHOICES
+
+
+class Cliente(models.Model):
+    nome = models.CharField("Nome", max_length=100)
+
+    def __str__(self):
+        return self.nome
+
+
+class Proposta(models.Model):
+    cliente = models.ForeignKey("Cliente", on_delete=models.CASCADE)
+    cpf = models.CharField("CPF", blank=True, max_length=14)
+    cnpj = models.CharField("CNPJ", blank=True, max_length=18)
+    endereco = models.CharField("Endereço", blank=True, max_length=200)
+    bairro = models.CharField("Bairro", blank=True, max_length=100)
+    cidade = models.CharField("Cidade", blank=True, max_length=100)
+    estado = models.CharField(
+        max_length=2, null=True, blank=True, choices=STATE_CHOICES
+    )
+    data = models.DateField("Data de Emissão")
+    VALIDADE_CHOICES = (
+        ("Quinze", "15 (Quinze)"),
+        ("Trinta", "30 (Trinta)"),
+        ("Sessenta", "60 (Sessenta)"),
+    )
+
+    validade = models.CharField(
+        "Validade", blank=True, max_length=200, choices=VALIDADE_CHOICES
+    )
+    valor = models.DecimalField("Valor a pagar", max_digits=8, decimal_places=2)
+    CATEGORY_CHOICES = (
+        ("Atrasado", "Atrasado"),
+        ("AVencer", "Á Vencer"),
+        ("Pago", "Pago"),
+    )
+
+    situacao = models.CharField("Situação", max_length=200, choices=CATEGORY_CHOICES)
+    pago = models.BooleanField(default=False)
+
+    dominio = models.CharField(
+        "Dominio do Site",
+        blank=True,
+        max_length=200,
+        help_text="URL - exe: seudominio.com.br",
+    )
+    dominio_login = models.CharField(
+        "Acesso ao Admin",
+        max_length=200,
+        blank=True,
+        help_text="URL - exe: seudominio.com.br/admin",
+    )
+    login = models.CharField("Login", max_length=30, blank=True)
+    senha = models.CharField("Senha", max_length=30, blank=True)
+
+    def imprimir(self):
+        return mark_safe(
+            """<a href=\"/proposta/%s/\" target="_blank"><img src=\"/static/images/b_print.png\"></a>"""
+            % self.id
+        )
+
+    class Meta:
+        ordering = ["-data"]
+        verbose_name = "Proposta"
+        verbose_name_plural = "Propostas"
+
+    def __str__(self):
+        if self.cliente.nome:
+            return self.cliente.nome
+        else:
+            return self.custom_alias_name
